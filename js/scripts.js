42819c935ca5fa497935a701c28e4d3ad1bbb45c
hiichloee <reta1iate@hotmail.com>
2024-05-12 03:58:54 +1000

    4th draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,41 @@
 
-// index.html
-//  Initialize Swiper 
-var swiper = new Swiper(".bannerSwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+
+// User validation
+// get login status and update navigation bar buttons
+function updateNavbarForLoggedInUser(userInfo) {
+    document.getElementById('loginBtn').style.display = 'none'; // hidding Login Button
+    const userBtn = document.getElementById('userBtn');
+    userId.innerText = userInfo.username; // assuming the userInfo contains the username
+    userBtn.classList.remove('d-none'); // display username info button
+}
+
+function updateNavbarForLoggedOutUser() {
+    document.getElementById('userBtn').style.display = 'none'; 
+    document.getElementById('loginBtn').style.display = 'block'; 
+}
+
+// test
+const userInfo = { username: 'test1' }; 
+updateNavbarForLoggedInUser(userInfo);
+
+
+
+// Sidecanvas
+$("#categorySearch").on("input", function() {
+    var searchText = $(this).val().toLowerCase();
+    $(".list-group-item.category").each(function() {
+        var categoryText = $(this).text().toLowerCase();
+        if (categoryText.indexOf(searchText) === -1) {
+            $(this).hide();
+        } else {
+            $(this).show();
+        }
+    });
 });
 
-var swiper2 = new Swiper(".oneSwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-    },
-});
+// Calculate and display number of items
+var numberOfItem = $('#itemCard').children().length;
+$('#itemCount').text(numberOfItem + ' Items');
+
+
 
