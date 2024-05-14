77af1b1ae802e23cc79d9b868e0a1b116f5bcf8a
ziqinzhao2024 <michaelzhao@nbnco.com.au>
2024-05-14 21:15:34 +1000

    update route js

--- routers/router.js
+++ routers/router.js
@@ -1,18 +1,32 @@
 var exports = require('express');
 
 let router = exports.Router();
-let controller = require('../controllers/controller');
+let item_controller = require('../controllers/item_controller');
+let user_controller = require('../controllers/user_controller');
 
-router.post('/', (req, res) => {
-    controller.postCat(req, res);
+router.post('/item', (req, res) => {
+    item_controller.postItem(req, res);
 });
 
-router.get('/', (req, res) => {
-    controller.getAllCats(req, res);
+router.get('/item', (req, res) => {
+    item_controller.getAllItems(req, res);
 });
 
-router.delete('/', (req, res) => {
-    controller.deleteCat(req, res);
+router.delete('/item', (req, res) => {
+    item_controller.deleteItem(req, res);
 });
+
+router.post('/user', (req, res) => {
+    item_controller.postUser(req, res);
+});
+
+router.get('/user', (req, res) => {
+    user_controller.getAllUsers(req, res);
+});
+
+router.delete('/user', (req, res) => {
+    user_controller.deleteUser(req, res);
+});
+
 
 module.exports = router;
