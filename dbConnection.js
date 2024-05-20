1bba4ef96b290920ea28ffba8b556dae4f4e6551
ziqinzhao2024 <michaelzhao@nbnco.com.au>
2024-05-20 22:04:23 +1000

    update uri

--- dbConnection.js
+++ dbConnection.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://huazhao9824:WRYS0Mu3UrajvKFA@cluster0.tip0dat.mongodb.net/?retryWrites=true&w=majority&appName=cluster0";
+const uri = 'mongodb+srv://s217590332:BZhdv5xBUN1lIP2W@sit725.gjex4b9.mongodb.net/?retryWrites=true&w=majority&appName=sit725';
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
