f0baf5f022f08954d2513314991d69e85135b24c
ziqinzhao2024 <michaelzhao@nbnco.com.au>
2024-05-25 22:41:35 +1000

    fix typo

--- routers/orderRoutes.js
+++ routers/orderRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-  const OrderController = require('../controllers/orderController')(db);
+  const orderController = require('../controllers/orderController')(db);
   
   router.post('/', orderController.createOrder);
   router.put('/:id', orderController.updateOrder);
